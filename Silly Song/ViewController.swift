690a47932f6957ac0cfef0374c8594f3b55e40a5
Jeff <jeffdeagostino@gmail.com>
2017-07-27 22:00:31 -0400

    commit #1
    Signed-off-by: Jeff <jeffdeagostino@gmail.com>

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joined(separator: "\n")
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +31,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+    }
+    
+    
 }
+
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    //var fullName = fullName
+    let shortName = shortNameForName(name: fullName)
+    
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return String(lyrics)
+}
+func shortNameForName(name: String) -> String {
+    let index = name.lowercased().characters.index { "aeiou".characters.contains($0) }
+    var str = name.substring(with: (index != nil ? index! : name.endIndex)..<name.endIndex).lowercased()
+    
+    return String(str.characters)
+    
+    
+}
+
+
+
+
+
+
+
+
+
 
